01f19c537e007cf1923a3bc7574f10ab07f203f7
nunumick <nunumick@gmail.com>
2023-07-14 15:45:09 +0800

    remove years and permalink

--- dist/_create-archive-files.py
+++ dist/_create-archive-files.py
@@ -6,7 +6,6 @@ import pathlib
 import requests
 
 archive_types = {
-    'year': 'years',
     'tag': 'tags',
     'category': 'categories'
 }
@@ -17,9 +16,9 @@ input_options = ['delete_archives', 'delete_archives_folder']
 def create_front_matter(archive_s_form, archive_p_form, archive_item_value, archive_value_escaped):
     front_matter_template = f'''---
 title: {archive_item_value}
-{archive_s_form}: "{archive_item_value}"
+type: {archive_s_form}
+{archive_s_form}: {archive_item_value}
 layout: archive-{archive_p_form}
-permalink: "{archive_s_form}/{archive_value_escaped}"
 ---
 '''
     return front_matter_template
